4a4fe31e546bb72c0b52c938e3bf2d4ce9e0d0ca
Wajid Khan <wajidkhan88@gmail.com>
2020-10-26 12:26:49 +0530

    added prompt

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ import numpy
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
+    number = input("enter a number")
+    print("you entered : ", number)
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
