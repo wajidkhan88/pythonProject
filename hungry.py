f544751d6c17168ffd80eaab5b648e397aa56a5b
Wajid Khan <wajidkhan88@gmail.com>
2020-10-28 09:54:55 +0530

    four initial functions

--- hungry.py
+++ hungry.py
@@ -1,7 +1,8 @@
-hungry = input("are you hungry")
-if hungry == "yes":
-    print("eat samosa")
-    print("eat pizza")    print("eat burger")
-
-else:
-    print("do your homework")
+def add(a,b):
+    pass
+def substract(a,b):
+    pass
+def multiply(a,b):
+    pass
+def devide(a,b):
+    pass
